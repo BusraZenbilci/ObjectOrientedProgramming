155ced05cff3cd2f59c55d94884df5fcb781e50c
Büşra Zenbilci <88310614+BusraZenbilci@users.noreply.github.com>
2023-07-24 14:50:53 +0300

    Delete Assignment01 directory

--- Assignment01/src/InvalidRankException.java
+++
@@ -1,9 +0,0 @@
-public class InvalidRankException extends RuntimeException {
-
-    private int rank;
-
-    @Override
-    public String toString() {
-        return "InvalidRankException: " + rank;
-    }
-}
